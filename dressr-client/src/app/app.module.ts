5c712b10d411ec6a54799db0cab1be9064aba26d
MJHR <majohr89@gmail.com>
2018-10-11 16:37:34 +0200

    validation signup login and css

--- dressr-client/src/app/app.module.ts
+++ dressr-client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 // ---- Components ---- //
@@ -13,6 +15,9 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 // ---- Guards ---- //
+
+// ---- Services ---- //
+
 
 // ---- Routes ---- //
 const routes: Routes = [
@@ -32,7 +37,8 @@ const routes: Routes = [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- dressr-client/src/app/components/signup/signup.component.ts
+++ dressr-client/src/app/components/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  feedbackEnabled = false;
+  error = null;
+  processing = false;
+  username: string;
+  password: string;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  submitForm(form) {
+    this.error = '';
+    this.feedbackEnabled = true;
+    if (form.valid) {
+      this.processing = true;
+      const data = {
+        username: this.username,
+        password: this.password
+      };
+      this.authService.login(data)
+        .then(() => {
+          this.router.navigate(['/']);
+        })
+        .catch((err) => {
+          this.error = err.error.code || 'unexpected'; // :-)
+          this.processing = false;
+          this.feedbackEnabled = false;
+        });
+    }
+  }
 }
+
