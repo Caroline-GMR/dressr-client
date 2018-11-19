e7f106f0ffdb12d7eaed4608d9c4bce7a403b076
MJHR <majohr89@gmail.com>
2018-11-19 13:13:09 +0000

    weather coords established

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -2,12 +2,6 @@ import { Component, OnInit } from '@angular/core';
 //import { last } from '../../../../node_modules/@angular/router/src/utils/collection';
 import { WeatherService } from '../../services/weather.service';
 
-const options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
-
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
@@ -15,38 +9,79 @@ const options = {
 })
 
 export class WeatherComponent implements OnInit {
-  
   feedbackEnabled = false;
   error = null;
   processing = false;
+  geolocationPosition;
+  lat;
+  lng;
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
-    const promise = new Promise((resolve, reject) => {
-      resolve(this.getCurrentLocation)
-    });
-    
-    promise.then((value) => {
-      // CALL THE SERVICE
-      this.weatherService.getWeather(value[0], value[1])
-    });
 
-  }
+    if (window.navigator && window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(
+          position => {
+              this.geolocationPosition = position,
+              this.lat = this.geolocationPosition.coords.latitude,
+              this.lng = this.geolocationPosition.coords.longitude,
+              this.weatherService.getWeather(this.lat, this.lng);
+                  console.log(position)
+                  console.log(position.coords.latitude)
+                  console.log(this.lng)
+          },
+          error => {
+              switch (error.code) {
+                  case 1:
+                      console.log('Permission Denied');
+                      break;
+                  case 2:
+                      console.log('Position Unavailable');
+                      break;
+                  case 3:
+                      console.log('Timeout');
+                      break;
+              }
+          }
+      );
+    };
 
-  getCurrentLocation() {
-    function success(pos) {
-      var crd = pos.coords;
-       return [crd.latitude, crd.longitude]
     
-    }
+    // this.getCurrentLocation(this.lat, this.lng);
     
-    function error(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
+    // this.weatherService.getWeather(this.lat, this.lng)
     
-    navigator.geolocation.getCurrentPosition(success, error, options);
+    
+    // const promise = new Promise((resolve, reject) => {
+      //   resolve(this.getCurrentLocation)
+      // });
+      
+      // promise.then((value) => {
+        //   // CALL THE SERVICE
+        //   this.results = this.weatherService.getWeather(value[0], value[1])
+        //   console.log(value[0])
+        // });
+        
+      }
+      
+  // getCurrentWeather() {
+  //   this.results = this.weatherService.getWeather(this.lat, this.lng)
+  //   console.log(this.results)
+  // }
+  // getCurrentLocation() {
+  //   function success(pos) {
+  //     var crd = pos.coords;
+  //     return [crd.latitude, crd.longitude]
+      
+  //   }
+    
+  //   function error(err) {
+  //     console.warn(`ERROR(${err.code}): ${err.message}`);
+  //   }
+    
+  //   navigator.geolocation.getCurrentPosition(success, error, options);
 
-  }
+  // }
   
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '../../../node_modules/@angular/common/http';
+import { HttpClient, HttpHeaderResponse } from '../../../node_modules/@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  private weather: any = {};
 
   constructor(private httpClient: HttpClient) { }
 
@@ -12,7 +13,7 @@ export class WeatherService {
     // Find hopw to get the current location (hint: try with the getCurrentLocation browser API)
     // form the ngOnInit method in the component call this service method, passing the lat and long
     // then in the component resolve the promise and do wathever you want with teh data
-    return this.httpClient.get(`https://api.darksky.net/forecast/e5b395c762d79d7c9d230efa2f4b4c09/${lat},${lng}`)
+    return this.httpClient.get(`http://crossorigin.me/https://api.darksky.net/forecast/e5b395c762d79d7c9d230efa2f4b4c09/${lat},${lng}`)
       .toPromise();
   }
 }
